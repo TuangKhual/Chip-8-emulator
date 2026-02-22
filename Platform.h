7a71fd291c9759a627569b55452c19b345e80ba7
TuangKhual <tuangkhual07@gmail.com>
2026-02-22 01:25:41 -0500

    Update Platform.h

--- Platform.h
+++ Platform.h
@@ -3,16 +3,20 @@
 #include <cstdint>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
 
 class Platform{
     public:
         Platform(char const* title, int windowWidth, int windowHeight, int textureWidth, int textureHeight);
         ~Platform();
         void update(void const* buffer, int pitch);
-        bool processInput(uint8_t* keys);
-
+        bool processInput(uint8_t* keys); 
+        void soundPlayer(bool active);     
     private:
         sf::RenderWindow window;
         sf::Texture texture;
         sf::Sprite sprite;
+        sf::SoundBuffer soundBuffer;
+        sf::Sound beep;  
+
 };
